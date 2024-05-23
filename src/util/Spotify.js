e39f236aea028aff9895827ecade64f4113f0b63
Laurence <laurencemurrin@gmail.com>
2024-05-23 20:47:40 +0100

    Redirect URL set to localhost.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = "f1028f2b0da94f0a98ca75dc4c08b699"; // Insert client ID here.
-const redirectUri = "https://lmurr248.github.io/jammming/"; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
+const redirectUri = "http://localhost:3000/"; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken;
 
 const Spotify = {
